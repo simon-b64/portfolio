7bff1f95cad98d0d7647704c202dd325e3891c96
Simon B. <47189894+simon-b64@users.noreply.github.com>
2026-01-10 13:59:30 +0100

    feat: Added base route and font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeView } from './view/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: HomeView
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
