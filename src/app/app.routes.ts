5e412c34d2e0505e0b21339f63551d7ba47a72d0
Simon B. <47189894+simon-b64@users.noreply.github.com>
2026-01-12 11:14:56 +0100

    feat: Added blog page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeView } from './view/home/home';
 import { NotFoundView } from './view/not-found/not-found';
+import { BlogView } from './view/blog/blog';
 
 export const routes: Routes = [
     {
+        title: 'Simon Buchinger - Software Engineer',
         path: '',
         pathMatch: 'full',
         component: HomeView
+    },
+    {
+        title: 'Blog | Simon Buchinger',
+        path: 'blog',
+        component: BlogView
     },
     {
         path: '**',
