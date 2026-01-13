1898bff2e2e7c9d79a454b638ae8eaf88947eee5
Simon B. <47189894+simon-b64@users.noreply.github.com>
2026-01-13 10:00:19 +0100

    feat: Added example blog implementation

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,9 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { TranslateService } from '@ngx-translate/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
     selector: 'app-root',
@@ -11,11 +14,15 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class App {
     private translateService = inject(TranslateService);
+    private library = inject(FaIconLibrary);
+
 
     constructor() {
         this.translateService.addLangs(['de', 'en']);
         this.translateService.setFallbackLang('en');
         this.translateService.use('en');
+
+        this.library.addIconPacks(fas, far);
     }
 
 }
--- src/app/view/blog/blog.ts
+++ src/app/view/blog/blog.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-blog',
     imports: [
-        FaIconComponent
+        FaIconComponent,
+        DatePipe
     ],
     templateUrl: './blog.html',
     styleUrl: './blog.scss',
@@ -13,4 +15,18 @@ import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
 export class BlogView {
     protected readonly faPenToSquare = faPenToSquare;
 
+    protected blogPost  = signal([
+        {
+            icon: ['fas', 'circle-nodes'] as any,
+            title: 'Personal Website 2026',
+            date: '2026-01-13',
+            content: 'This year I decided to start the tradition of reworking my personal website every year. Whilst implementing this I wanted to start using newer technologies and using the new angular 21 features. I have also decided to use a blog format to document my journey throughout the year.'
+        },
+        {
+            icon: ['far', 'pen-to-square'] as any,
+            title: 'First example blog post',
+            date: '2024-02-20',
+            content: 'This is the content of the second blog post.'
+        }
+    ])
 }
