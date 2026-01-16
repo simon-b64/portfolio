c611b651cae4630ede802bb1ecebd34893e4a147
Simon B. <47189894+simon-b64@users.noreply.github.com>
2026-01-16 22:08:56 +0100

    feat: Updated blog list for mobile optimisation

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,6 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { TranslateService } from '@ngx-translate/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
     selector: 'app-root',
@@ -14,15 +11,11 @@ import { far } from '@fortawesome/free-regular-svg-icons';
 })
 export class App {
     private translateService = inject(TranslateService);
-    private library = inject(FaIconLibrary);
-
 
     constructor() {
         this.translateService.addLangs(['de', 'en']);
         this.translateService.setFallbackLang('en');
         this.translateService.use('en');
-
-        this.library.addIconPacks(fas, far);
     }
 
 }
--- src/app/view/blog/blog.ts
+++ src/app/view/blog/blog.ts
@@ -1,15 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+interface BlogPost {
+    id: string;
+    title: string;
+    date: Date;
+    content: string;
+    icon: string;
+}
 
 @Component({
     selector: 'app-blog',
     imports: [
         FaIconComponent,
         DatePipe,
-        RouterLink
+        RouterLink,
+        NgOptimizedImage
     ],
     templateUrl: './blog.html',
     styleUrl: './blog.scss',
@@ -17,20 +26,20 @@ import { RouterLink } from '@angular/router';
 export class BlogView {
     protected readonly faPenToSquare = faPenToSquare;
 
-    protected blogPost  = signal([
+    protected blogPost  = signal<BlogPost[]>([
         {
-            id: 2,
-            icon: ['fas', 'circle-nodes'] as any,
+            id: '2',
+            icon: '/assets/icons/circle-nodes.svg',
             title: 'Personal Website 2026',
-            date: '2026-01-13',
+            date: new Date(),
             content: 'This year I decided to start the tradition of reworking my personal website every year. Whilst implementing this I wanted to start using newer technologies and using the new angular 21 features. I have also decided to use a blog format to document my journey throughout the year.'
         },
         {
-            id: 1,
-            icon: ['far', 'pen-to-square'] as any,
+            id: '1',
+            icon: '/assets/icons/circle-nodes.svg',
             title: 'First example blog post',
-            date: '2024-02-20',
+            date: new Date(),
             content: 'This is the content of the second blog post.'
         }
-    ])
+    ]);
 }
