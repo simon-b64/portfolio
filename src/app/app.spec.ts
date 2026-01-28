9ed4d411221ff348f6d620f71e29b8055a294ca6
Simon B. <47189894+simon-b64@users.noreply.github.com>
2026-01-28 11:55:27 +0100

    fix: Fixed pseudo app test

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [App],
+            providers: [
+                provideRouter([]),
+                provideTranslateService({
+                    lang: 'en',
+                    fallbackLang: 'en'
+                })
+            ]
+        }).compileComponents();
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(App);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
+    });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, portfolio');
-  });
+    it('should render title', async () => {
+        const fixture = TestBed.createComponent(App);
+        await fixture.whenStable();
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('nav')).toBeTruthy();
+    });
 });
